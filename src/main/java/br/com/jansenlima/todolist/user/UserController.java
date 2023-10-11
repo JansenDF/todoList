f79ed03dbd627ec11da92e14bcfefe6aff3686a1
Jansen Lima <jansen.lima.df@gmail.com>
2023-10-10 21:28:56 -0300

    Adicionado restrição username unico no database e adicionado retorno http durante processo de criação de user.

--- src/main/java/br/com/jansenlima/todolist/user/UserController.java
+++ src/main/java/br/com/jansenlima/todolist/user/UserController.java
@@ -1,6 +1,8 @@
 package br.com.jansenlima.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +15,15 @@ public class UserController {
   private IUserRepository userRepository;
 
   @PostMapping("/")
-  public UserModel createUser(@RequestBody UserModel userModel) {
+  public ResponseEntity createUser(@RequestBody UserModel userModel) {
+    var user = this.userRepository.findByUsername(userModel.getUsername());
+    if (user != null) {
+      //Mensagem de Erro  - Status Code
+      return ResponseEntity
+        .status(HttpStatus.BAD_REQUEST)
+        .body("Usuário já existe");
+    }
     var userCreated = this.userRepository.save(userModel);
-    return userCreated;
+    return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
   }
 }

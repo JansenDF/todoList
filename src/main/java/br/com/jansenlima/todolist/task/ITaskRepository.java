ca01f91f499ee9bcc1ef0cb78e05ef10f43e538b
Jansen Lima <jansen.lima.df@gmail.com>
2023-10-12 11:25:29 -0300

    Adicionado metodo para criação de nova task.

---
+++ src/main/java/br/com/jansenlima/todolist/task/ITaskRepository.java
@@ -0,0 +1,6 @@
+package br.com.jansenlima.todolist.task;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {}

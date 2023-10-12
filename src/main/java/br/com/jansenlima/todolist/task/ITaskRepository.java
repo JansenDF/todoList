8bf8c334b59caeb579675634567d6c1061a12632
Jansen Lima <jansen.lima.df@gmail.com>
2023-10-12 20:14:59 -0300

    Adicionado rota para obter tasks do usuário logado.

--- src/main/java/br/com/jansenlima/todolist/task/ITaskRepository.java
+++ src/main/java/br/com/jansenlima/todolist/task/ITaskRepository.java
@@ -1,6 +1,9 @@
 package br.com.jansenlima.todolist.task;
 
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {}
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+  List<TaskModel> findByIdUser(UUID idUser);
+}

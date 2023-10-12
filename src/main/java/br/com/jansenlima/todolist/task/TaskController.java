d07f0fe773d14187baf791e6bc2a92280cccb133
Jansen Lima <jansen.lima.df@gmail.com>
2023-10-12 20:02:59 -0300

    Adicionado validação da data de início e término das tasks.

--- src/main/java/br/com/jansenlima/todolist/task/TaskController.java
+++ src/main/java/br/com/jansenlima/todolist/task/TaskController.java
@@ -1,8 +1,11 @@
 package br.com.jansenlima.todolist.task;
 
 import jakarta.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,13 +18,33 @@ public class TaskController {
   private ITaskRepository taskRepository;
 
   @PostMapping("/")
-  public TaskModel create(
+  public ResponseEntity create(
     @RequestBody TaskModel taskModel,
     HttpServletRequest request
   ) {
     var idUser = request.getAttribute("idUser");
     taskModel.setIdUser((UUID) idUser);
+    var currentDate = LocalDateTime.now();
+
+    //Verificar se startAt e endAt estão no passado
+    if (
+      currentDate.isAfter(taskModel.getStartAt()) ||
+      currentDate.isAfter(taskModel.getEndAt())
+    ) {
+      return ResponseEntity
+        .status(HttpStatus.BAD_REQUEST)
+        .body(
+          "A data de início ou data de término deve ser maior do que a data atual"
+        );
+    }
+
+    //Verificar se startAt é maior do que endAt
+    if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+      return ResponseEntity
+        .status(HttpStatus.BAD_REQUEST)
+        .body("A data de início deve ser menor do que a data de término");
+    }
     var task = this.taskRepository.save(taskModel);
-    return task;
+    return ResponseEntity.status(HttpStatus.OK).body(task);
   }
 }
